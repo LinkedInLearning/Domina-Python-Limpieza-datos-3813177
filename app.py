3947dd6016b06b84193c2adbf2126ccce941431b
Lincy <lincgr@gmail.com>
2024-04-13 12:19:15 -0600

    01_03 Visualización de datos para identificar valores faltantes o atípicos con Python

---
+++ app.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+datos = {
+    "propietario": ["Juan Pérez", "María López", "Carlos Gómez", "Ana Rodríguez", "Luis Martínez", "Laura Pérez", "Pablo Ruiz", "Marta Gutiérrez", "Manuel González", "Sofía Díaz", "Laura Martínez", "Carlos Vargas"],
+    "metros": [200, None, 500, None, 300, None, 400, None, 600, None, 250, None],
+    "propiedad_ocupada": ["S", "S", "N", "S", "S", "S", "S", "N", "S", "S", "S", "S"],
+    "num_cuartos": [3, 2, 4, 1, 5, 3, 4, 2, 6, 3, 4, 2],
+    "tipo_prop": ["Casa", "Apartamento", "Casa", "Apartamento", "Casa", "Apartamento", "Casa", "Apartamento", "Casa", "Apartamento", "Casa", "Apartamento"],
+    "ubicacion": ["Alajuela", "San José", "Cartago", "Heredia", "Guanacaste", "Puntarenas", "San José", "Cartago", "Limón", "Alajuela", "Heredia", "Alajuela"],
+    "año_construccion": [1995, 2010, 1980, 2015, 2005, 2018, 2000, 1990, 2008, 2012, 2007, 2019],
+    "valor_prop": [250000, 180000, 400000, 120000, 320000, 200000, 350000, 150000, 450000, 220000, 280000, 130000],
+    "num_baños": [2, 1, 3, 1, None, 1.5, 2.5, 1.5, 3.5, 2, None, 1],
+    "estado_prop": ["Buen estado", "Buen estado", "Necesita reparaciones", "Buen estado", "Buen estado", "Buen estado", "Buen estado", "Necesita reparaciones", "Buen estado", "Buen estado", "Buen estado", "Nuevo"],
+    "tipo_construccion": ["Tradicional", "Moderna", "Tradicional", "Moderna", "Tradicional", "Moderna", "Tradicional", "Tradicional", "Moderna", "Moderna", "Tradicional", "Moderna"],
+    "servicios_disp": ["Garaje", "Gimnasio", "Jardín", "Estacionamiento", "Garaje", "Piscina", "Terraza", "Estacionamiento", "Piscina", "Estacionamiento", "Terraza", "Balcón"],
+    "comodidades": ["Terraza", "Balcón", "Chimenea", "Balcón", "Terraza", "Vista al parque", "Chimenea", "Balcón", "Terraza", "Balcón", "Patio", "Vista a la ciudad"],
+    "num_garajes": [2, 1, 3, 1, 2, None, 2, 1, 3, 1, None, 1]
+}
+df = pd.DataFrame(datos)

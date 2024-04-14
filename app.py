ac11ffb94b3946abc99535bcee12511fdc5dca8b
Lincy <lincgr@gmail.com>
2024-04-14 10:29:58 -0600

    02_08 Eliminar columnas de un dataframe con valores faltantes con Python

---
+++ app.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+datos = {
+    "propietario": ["Juan Pérez", "María López", "Carlos Gómez", "Ana Rodríguez", "Luis Martínez", "Laura Pérez", "Pablo Ruiz", "Marta Gutiérrez", "Manuel González", "Sofía Díaz", "Laura Martínez", "Carlos Vargas"],
+    "metros": [None, None, 500, None, None, None, 400, None, None, None, 250, None],
+    "propiedad_ocupada": ["S", "S", "N", "S", "S", "S", "S", "N", "S", "S", "S", "S"],
+    "num_cuartos": [3, 2, 4, 1, 5, 3, 4, 2, 6, 3, 4, 2],
+    "tipo_prop": ["Casa", "Apartamento", "Casa", "Apartamento", "Casa", "Apartamento", "Casa", "Apartamento", "Casa", "Apartamento", "Casa", "Apartamento"],
+    "ubicacion": ["Alajuela", "San José", "Cartago", "Heredia", "Guanacaste", "Puntarenas", "San José", "Cartago", "Limón", "Alajuela", "Heredia", "Alajuela"],
+    "año_construccion": [1995, 2010, 1980, 2015, 2005, 2018, 2000, 1990, 2008, 2012, 2007, 2019],
+    "valor_prop": [250000, 180000, 400000, 120000, 320000, 200000, 350000, 150000, 450000, 220000, 280000, 130000],
+    "num_baños": [2, 1, 3, 1, None, 1.5, 2.5, 1.5, 3.5, 2, None, 1],
+    "estado_prop": ["Buen estado", "Buen estado", "Necesita reparaciones", "Buen estado", "Buen estado", "Buen estado", "Buen estado", "Necesita reparaciones", "Buen estado", "Buen estado", "Buen estado", "Nuevo"],
+    "tipo_construccion": ["Tradicional", "Moderna", "Tradicional", "Moderna", "Tradicional", "Moderna", "Tradicional", "Tradicional", "Moderna", "Moderna", "Tradicional", "Moderna"],
+    "servicios_disp": ["Garaje", "Gimnasio", "Jardín", "Estacionamiento", "Garaje", "Piscina", "Terraza", "Estacionamiento", "Piscina", "Estacionamiento", "Terraza", "Balcón"],
+    "comodidades": [None, "Balcón", None, None, None, None, None, None, "Terraza", None, "Patio", None],
+    "num_garajes": [2, 1, 3, 1, 2, None, 2, 1, 3, 1, None, 1]
+}
+
+df = pd.DataFrame(datos)
+
+print("Número de filas y columnas", df.shape)
+
+print("Valores faltantes por columna: \n", df.isnull().sum())   
+
+df = df.dropna(axis=1, thresh=len(df) - 8)
+
+print(df.info())
